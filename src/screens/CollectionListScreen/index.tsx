2a4335ccb52bba1fe268f21c94ae96394024afe7
poby123 <i2pir@naver.com>
2023-10-16 13:47:14 +0900

    feat: collection top navigator

--- src/screens/CollectionListScreen/index.tsx
+++ src/screens/CollectionListScreen/index.tsx
@@ -1,7 +1,4 @@
-import { ButtonIcon } from '@gluestack-ui/themed';
 import CollectionList from '~/components/CollectionList';
-import Header from '~/components/Header';
-import { SearchIcon } from '~/components/Icon/SearchIcon';
 import SafeView from '~/components/SafeView';
 import demo from './demo.json';
 
@@ -9,9 +6,6 @@ export const CollectionListScreen = () => {
   const { collections } = JSON.parse(JSON.stringify(demo));
   return (
     <SafeView>
-      <Header
-        right={<ButtonIcon size="xl" color="$primary900" as={SearchIcon} />}
-      />
       <CollectionList collections={collections} />
     </SafeView>
   );
